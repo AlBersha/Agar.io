ee72b026c41ae1225774b041999b4a6ddb104823
Oleksandra Bershatska <oleksandra.bershatska@solarwinds.com>
2021-12-17 15:50:06 +0200

    scoring added

--- Assets/Scripts/Eat.cs
+++ Assets/Scripts/Eat.cs
@@ -1,20 +1,24 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Eat : MonoBehaviour
 {
     public string Tag;
     public float Increase;
 
+    private int Score = 0;
+    public Text ScoreText;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag(Tag))
         {
             transform.localScale += new Vector3(Increase, Increase, Increase);
+            Score += 10;
+            ScoreText.text = "Score : " + Score;
+            
             Destroy(other.gameObject);
+            
         }
     }
 }
