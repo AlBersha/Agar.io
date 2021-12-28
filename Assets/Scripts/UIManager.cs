874d7e0ec781c47889656b26d38aba9a9dc71c4b
Nikita <nikita.molodtsov21@gmail.com>
2021-12-28 03:00:16 +0200

    Adding server handling

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class UIManager : MonoBehaviour
+    {
+        public static UIManager instance;
+
+        public GameObject startMenu;
+        public InputField usernameField;
+
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else if (instance != this)
+            {
+                Debug.Log("Instance already exists, destroying object!");
+                Destroy(this);
+            }
+        }
+
+        public void ConnectToServer()
+        {
+            startMenu.SetActive(false);
+            usernameField.interactable = false;
+            Client.instance.ConnectToServer();
+        }
+    }
+}
